b977a20eca5e698519c53900e86a489bdb06f133
YMG WORKS <59523977+YMG-CODE@users.noreply.github.com>
2025-09-14 23:56:30 +0900

    Add files via upload

---
+++ keyboards/ete_point_rp2040/theendpoint_rp2040/keymaps/trackball_LF_RB_comb/rules.mk
@@ -0,0 +1,16 @@
+ENCODER_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+# Optical sensor driver for trackball.
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_SPLITS += yes
+
+
+
+# Include common library
+SRC += lib_combine/ETE/ETE.c
+SRC += drivers/pmw3360/pmw3360.c
+POINTING_DEVICE_DRIVER = pmw3360
+
+QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
+
